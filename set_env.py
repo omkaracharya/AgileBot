91b56b981d06569fd63d4bdcb22e83c407cc6bb9
awatve <awatve@ncsu.edu>
2017-10-19 19:04:43 -0400

    Reorganize directory structure

--- set_env.py
+++ set_env.py
@@ -1,7 +1,7 @@
 import os
 
-os.environ.setdefault("AGILEBOT_ID", "U7LC7HRHT")
-os.environ.setdefault("AGILEBOT_TOKEN", "xoxb-258415603605-BcXTm6BJTkxs5CxIAqt04Ezz")
+os.environ.setdefault("AGILEBOT_ID", "")
+os.environ.setdefault("AGILEBOT_TOKEN", "")
 os.environ.setdefault("RALLY_USERNAME", "")
 os.environ.setdefault("RALLY_PASSWORD", "")
 os.environ.setdefault("RALLY_APIKEY", "")
