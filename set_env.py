1fdcf039dfcc73da87c1424c7661f81f8082b314
Omkar Acharya <oachary@ncsu.edu>
2017-10-19 15:58:13 -0400

    Add environment variables

---
+++ set_env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("AGILEBOT_ID", "")
+os.environ.setdefault("AGILEBOT_TOKEN", "")
+os.environ.setdefault("RALLY_USERNAME", "")
+os.environ.setdefault("RALLY_PASSWORD", "")
+os.environ.setdefault("RALLY_APIKEY", "")
