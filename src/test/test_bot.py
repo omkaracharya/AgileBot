221a6d00f57117dd2a5c551412ee8e6500e20e7a
yvlele <yvlele@ncsu.edu>
2017-10-19 21:39:56 -0400

    Add fix for folder restructure and Python 3

--- src/test/test_bot.py
+++ src/test/test_bot.py
@@ -1,18 +1,15 @@
 import sys
 # import request
-# import unittest
-import mock
+from unittest.mock import patch
 # from nose.tools import assert_true
 
-sys.path.append("../src/application/")
-import agilebot
+sys.path.append("../src/application/main")
 
 
-@mock.patch('agilebot.os')
+@patch('agilebot.os')
 def test_env(mock_os):
     BOT_ID = mock_os.environ.get("BOT_ID")
     mock_os.environ.get.assert_called_with("BOT_ID")
-
 
 # @mock.patch('agilebot.slack_client')
 # def test_handle_command(mock_slack_client):
