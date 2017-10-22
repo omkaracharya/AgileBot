5b497043ec388ece7b28995d81cd00f5f55b107a
oachary <oachary@ncsu.edu>
2017-10-22 17:15:05 -0400

    Update set_env.py

--- set_env.py
+++ set_env.py
@@ -1,11 +1,11 @@
 import os
 
-os.environ.setdefault("AGILEBOT_ID", "U7LC7HRHT")
-os.environ.setdefault("AGILEBOT_TOKEN", "xoxb-258415603605-BcXTm6BJTkxs5CxIAqt04Ezz")
+os.environ.setdefault("AGILEBOT_ID", "")
+os.environ.setdefault("AGILEBOT_TOKEN", "")
 os.environ.setdefault("RALLY_USERNAME", "")
 os.environ.setdefault("RALLY_PASSWORD", "")
 os.environ.setdefault("RALLY_APIKEY", "")
-os.environ.setdefault("CHROME_DRIVER_PATH", "C:/NCSU/Fall 17/CSC_510_SE/CSC-510-Project/chromedriver.exe")
-os.environ.setdefault("SLACK_URL", "https://csc510project.slack.com/")
-os.environ.setdefault("TESTER_EMAIL", "r132367@mvrht.net")
-os.environ.setdefault("TESTER_PASSWORD", "tester12345")
+os.environ.setdefault("CHROME_DRIVER_PATH", "")
+os.environ.setdefault("SLACK_URL", "")
+os.environ.setdefault("TESTER_EMAIL", "")
+os.environ.setdefault("TESTER_PASSWORD", "")
