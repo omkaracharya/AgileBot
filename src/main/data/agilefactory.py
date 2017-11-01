a9fc0f2ca899381376fe2d3127a0b099c1a90539
awatve <awatve@ncsu.edu>
2017-11-01 18:03:50 -0400

    Update action_builder.py

--- src/main/data/agilefactory.py
+++ src/main/data/agilefactory.py
@@ -4,20 +4,23 @@ from pyral import Rally
 from main.application.authority import get_authorized_connection_or_permission
 from main.data.environment import get_env
 
+rally = None
 
-class AgileFactory:
-    @staticmethod
-    def connect():
-        server = get_env("RALLY_SERVER")
-        user = get_env("RALLY_USER")
-        password = get_env("RALLY_PASSWORD")
-        apikey = get_env("RALLY_APIKEY")
-        rally = Rally(server, user, password, apikey)
-        return rally
 
-    @staticmethod
-    def factory():
-        grant = get_authorized_connection_or_permission()
-        if grant:
-            return grant
-        return AgileFactory.connect()
+def connect():
+    server = get_env("RALLY_SERVER")
+    user = get_env("RALLY_USER")
+    password = get_env("RALLY_PASSWORD")
+    apikey = get_env("RALLY_APIKEY")
+    rally = Rally(server, user, password, apikey)
+    return rally
+
+
+def get_instance():
+    grant = get_authorized_connection_or_permission()
+    if grant:
+        return grant
+    global rally
+    if not rally:
+        rally = connect()
+    return rally
