4a06838a9d5662360ee9fe6d515ef576be3bad90
Yogesh Lele <yvlele@ncsu.edu>
2017-11-14 01:44:25 -0500

    Groom backlog with points assigned tentatively (#3)
    * Add rally project name in connection
    
    * Comment flask app run
    
    * Update query to get zero point stories
    
    * Select random points for ungrommed stories
    
    * Add task and children count lookup
    
    * Update groom backlog mock data and interaction
    
    * Clean unit_test.py
    
    * Add relative path for fetching environment variables
    
    * Add pip3, networkx and user mode
    
    1. pip3 -> for Python3
    2. '--user' to install dependencies when the user does not have
    administrative privileges
    
    * Assign tentative points to stories not groomed
    
    1. Fetch stories with No Owner and No Plan estimate
    2. Topological sort the dependent queries
    3. Sort on expedite
    4. Assign points
    
    * Add date parsing before sort

--- src/main/application/groombacklog.py
+++ src/main/application/groombacklog.py
@@ -1,6 +1,7 @@
 # Use Case: groom backlog
 
 from datetime import datetime
+import networkx as nx
 
 from main.application.authority import get_action_authorized
 from main.data.commands import GROOMBACKLOG
@@ -11,11 +12,13 @@ class GroomBacklog:
     def __init__(self):
         self.date = datetime.today()
         self.command = GROOMBACKLOG
-        self.RESPONSE_HEADER = "Groomed Backlog:"
+        self.RESPONSE_HEADER = "Tentatively Groomed Backlog:"
         self.INVALID_RESPONSE = "\nNo stories in backlog to groom."
+        self.PER_USER_QUOTA = 13
+        self.STORY_BASE_POINTS = 5
+        self.TASK_BASE_POINTS = 3
 
     def get_response(self, user, all_users, request, rally):
-        # TODO: Implement service/ read from mock file
         if request:
             date = request[0]
             try:
@@ -28,17 +31,89 @@ class GroomBacklog:
         perform_action = get_action_authorized(self, self.groom)
 
         if backlog:
-            for story in backlog:
-                perform_action(story)
-
+            stories = [story for story in backlog]
+            sprint_quota = len(all_users) * self.PER_USER_QUOTA
+            perform_action(stories, sprint_quota)
             response += '\n' + '\n'.join(
-                ['Story #' + story.FormattedID + ': ' + story.Name + ' (Points: ' + str(story.PlanEstimate) + ')' for
-                 story in
-                 backlog])
+                ['Story #' + story.FormattedID + ': ' + story.Name +
+                 ' (Points: ' + str(story.PlanEstimate) + ')'
+                 for story in stories])
         else:
             response += self.INVALID_RESPONSE
         return response
 
-    # TODO : business logic to assign points
-    def groom(self):
-        pass
+
+    def groom(self, stories, quota_left):
+
+        ordering_criteria = [
+            'Expedite',
+            'whenCreated'
+        ]
+
+        chainedStories = [story for story in stories
+                          if story.Predecessors or story.Successors]
+        self.sort_stories(chainedStories)
+
+        independentStories = list(set(stories) - set(chainedStories))
+        for criterion in reversed(ordering_criteria):
+            self.sort_stories(independentStories, criterion=criterion)
+
+        tp_sorted_stories = self.sort_topological(chainedStories)
+
+        # self.print_stories(tp_sorted_stories)
+        # self.print_stories(independentStories)
+
+        quota_left = self.assign_points_wrapper(quota_left, tp_sorted_stories)
+        quota_left = self.assign_points_wrapper(quota_left, independentStories)
+
+
+    def assign_points_wrapper(self, quota_left, sorted_stories):
+        for sorted_story in sorted_stories:
+            if quota_left < 1:
+                break
+            quota_left = self.assign_points(sorted_story, quota_left)
+        return quota_left
+
+    def print_stories(self, stories):
+        print([story.Name + " | " + ("Expedite" if story.Expedite else "")
+               for story in stories])
+
+
+    def sort_topological(self, chainedStories):
+        G = nx.DiGraph()
+        for story in chainedStories:
+            # print(story.Predecessors)
+            if not G.has_node(story.FormattedID):
+                G.add_node(story.FormattedID, story=story)
+            else:
+                G.nodes[story.FormattedID]['story'] = story
+
+            if story.Predecessors:
+                [G.add_edge(dependent.FormattedID, story.FormattedID)
+                 for dependent in story.Predecessors]
+
+        sorted_stories = [G.nodes[key]['story'] for key in nx.topological_sort(G)]
+
+        return sorted_stories
+
+
+    def get_count(self, obj):
+        return (len(obj) if obj else 0)
+
+
+    def sort_stories(self, list, criterion='Expedite'):
+        if criterion is 'Expedite':
+            list.sort(key=lambda x: (0 if x.Expedite else 1))
+        if criterion is 'whenCreated':
+            list.sort(key=lambda x: datetime.strptime(x.CreationDate, '%Y-%m-%dT%H:%M:%S.%fZ'))
+
+    def assign_points(self, story, quota_left):
+        estimated_points = self.STORY_BASE_POINTS \
+                           + self.get_count(story.Successors) \
+                           + (self.get_count(story.Tasks) * self.TASK_BASE_POINTS)
+
+        if quota_left >= estimated_points:
+            story.PlanEstimate = estimated_points
+            quota_left -= estimated_points
+
+        return quota_left
